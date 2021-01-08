0cefa1781f2c0f1d9f517ed983f086b22f4c6db4
y4shiro <y4shironao@gmail.com>
2021-01-08 21:20:10 +0900

    03 素因数分解 挑戦問題初級

--- Factorization.scala
+++ Factorization.scala
@@ -1,19 +1,22 @@
 import scala.math.sqrt
 
 object Factorization extends App {
-  var target = 24
-  val maxDivisor = sqrt(target).toInt
+  def factorization(target: Int): Map[Int, Int] = {
+    val maxDivisor = sqrt(target).toInt
 
-  def factorizationRec(num: Int, divisor: Int, acc: Map[Int, Int]): Map[Int, Int] = {
-    if (divisor > maxDivisor) {
-      if (num == 1) acc else acc + (num -> 1)
-    } else if (num % divisor == 0) {
-      val nextAcc = acc + (divisor -> (acc.getOrElse(divisor, 0) + 1))
-      factorizationRec(num / divisor, divisor, nextAcc)
-    } else {
-      factorizationRec(num, divisor + 1, acc)
+    def factorizationRec(num: Int, divisor: Int, acc: Map[Int, Int]): Map[Int, Int] = {
+      if (divisor > maxDivisor) {
+        if (num == 1) acc else acc + (num -> 1)
+      } else if (num % divisor == 0) {
+        val nextAcc = acc + (divisor -> (acc.getOrElse(divisor, 0) + 1))
+        factorizationRec(num / divisor, divisor, nextAcc)
+      } else {
+        factorizationRec(num, divisor + 1, acc)
+      }
     }
+
+    factorizationRec(target, 2, Map())
   }
 
-  println(factorizationRec(target, 2, Map()))
+  println(factorization(24))
 }
